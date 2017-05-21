02605cdec50d33fef842f369ae0c3ead11ad0fa5
Casuneanu Andrei <casuneanu.andrei97@gmail.com>
2017-05-21 20:20:37 +0300

    fixed a bug

--- components/footer.js
+++ components/footer.js
@@ -1,17 +1,17 @@
 import Link from 'next/link'
- 
+
 const Footer = () => (
 
 	<div>
-	  <link href="static/img/favicon.ico" rel="shortcut icon"/>	  
-	  <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,700,700i|Raleway:300,400,500,700,800" rel="stylesheet"/> 
-	  <link href="static/lib/bootstrap/css/bootstrap.min.css" rel="stylesheet"/> 
+	  <link href="static/img/favicon.ico" rel="shortcut icon"/>
+	  <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,700,700i|Raleway:300,400,500,700,800" rel="stylesheet"/>
+	  <link href="static/lib/bootstrap/css/bootstrap.min.css" rel="stylesheet"/>
 	  <link href="static/lib/font-awesome/css/font-awesome.min.css" rel="stylesheet"/>
 	  <link href="static/lib/animate-css/animate.min.css" rel="stylesheet"/>
 	  <link href="static/css/style.css" rel="stylesheet"/>
 		<footer id="footer">
 	      <div className="container">
-	        <div classNameName="row">
+	        <div className="row">
 	          <div className="col-md-12">
 	            <div className="copyright">
 	              &copy; Copyright <strong>Imperial Theme</strong>. All Rights Reserved
